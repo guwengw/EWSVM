905d199f71575e1147dddc65d752f02ef7b2d7bc
guwengw <948228766@qq.com>
2022-04-27 21:00:53 +0800

    Add files via upload

---
+++ EWSVM.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  6 10:03:06 2021
+
+@author: guwen
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 20:30:18 2021
+
+@author: Administrator
+"""
+
+import os
+import numpy as np
+from sklearn.svm import SVC
+import utils 
+import h5py
+
+from sklearn.metrics import f1_score
+
+C = 10
+
+svc = SVC(C, kernel='rbf', gamma='auto', probability=True, random_state=2018)
+
+
+trainRate = 0.7
+
+algorithm = "EWSVM"
+
+data_list = ['mushroom']
+
+priors = [0.3,0.5,0.7]
+
+lambda_dir = {'mushroom': 0.1}
+
+for dataset in data_list: 
+    
+    n_unl = 800
+    
+    n_pos = 400
+
+    lambd = lambda_dir[dataset]
+    
+    x,t = utils.load_dataset(dataset)   
+    
+    x = utils.dataPreProcess(x)
+    
+    xtrain,xtest,ttrain,ttest = utils.trainTestSpilt(x,t,trainRate)
+    
+    xtest,ttest = utils.multiClassTest(xtest,ttest)
+    
+    result = []
+    
+    for prior in priors:
+        
+        print(prior)
+        
+        f1 = []
+        
+        for i in range(20):
+            
+            print(dataset + str(prior) + str(i))
+    
+            P,U = utils.positiveUnlabeledSpilt(xtrain,ttrain,n_unl,prior,n_pos)
+            
+            M = utils.computeCostMatrix(P,U)
+            
+            Gs = utils.sinkhornTransport(n_unl,n_pos,lambd,M)
+            
+            entro = utils.computeEntropy(Gs)
+            
+            X_train = np.r_[P, U]
+            y_train = np.r_[np.ones(len(P)), np.ones(len(U))*(-1)]
+            
+            weight = np.r_[np.ones(P.shape[0]),entro]
+            svc.fit(X_train, y_train, sample_weight=weight)
+            y_pred = svc.predict(xtest)
+            
+            errorE = len(np.where((y_pred == ttest ) == False)[0]) 
+            errorRateE = errorE / ttest.shape[0]
+            
+            f1EWSVM = f1_score(ttest,y_pred,average = 'micro')
+            f1.append(f1EWSVM)
+            
+        result.append(f1)
+    
+
---
+++ utils.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 17:01:53 2021
+
+@author: guwen
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 15:54:46 2021
+
+@author: Administrator
+"""
+
+import numpy as np
+import scipy as sp
+import ot
+from sklearn.datasets import load_svmlight_file
+from sklearn.model_selection import train_test_split
+
+
+def load_dataset(dataset):
+    if dataset == "mushroom":
+        x, t = load_svmlight_file("mushrooms.txt")
+        x = x.toarray()
+        x = np.delete(x, 77, 1)  
+        t[t == 1] = 1
+        t[t == 2] = -1   
+        
+    t = t.astype('int')
+    return x,t
+        
+    
+def dataPreProcess(x):
+    div = np.max(x, axis=0) - np.min(x, axis=0)
+    div[div == 0] = 1
+    x = (x - np.min(x, axis=0)) / div
+    return x
+
+def trainTestSpilt(x,t,trainRate):
+    trainSize = int(t.shape[0] * trainRate)
+    xtrain,xtest,ttrain,ttest = train_test_split(x, t, train_size=trainSize)
+    return xtrain,xtest,ttrain,ttest
+
+def multiClassTrain(xtrain,ttrain):
+    xtrain_p = xtrain[ttrain == 1]
+    ttrain_p = ttrain[ttrain == 1] 
+    
+    xtrain_n = xtrain[ttrain == -1]
+    ttrain_n = ttrain[ttrain == -1] 
+
+    xtrain_pn = np.r_[xtrain_p, xtrain_n]
+    ttrain_pn = np.r_[ttrain_p, ttrain_n]
+    
+    return xtrain_pn,ttrain_pn
+
+def multiClassTest(xtest,ttest):
+    xtest_p = xtest[ttest == 1]
+    ttest_p = ttest[ttest == 1] 
+    
+    xtest_n = xtest[ttest == -1]
+    ttest_n = ttest[ttest == -1] 
+
+    xtest_pn = np.r_[xtest_p, xtest_n]
+    ttest_pn = np.r_[ttest_p, ttest_n]
+    
+    return xtest_pn,ttest_pn
+    
+    
+def positiveUnlabeledSpilt(x,t,n_unl,prior,n_pos):
+    size_u_p = int(prior * n_unl)
+    size_u_n = n_unl - size_u_p
+
+    xp_t = x[t == 1]
+    tp_t = t[t == 1]    
+    
+    xp, xp_other, _, tp_o = train_test_split(xp_t, tp_t, train_size=n_pos)
+
+    xup, _, _, _ = train_test_split(xp_other, tp_o, train_size=size_u_p)
+
+
+    xn_t = x[t == -1]
+    tn_t = t[t == -1]
+    xun, _, _, _ = train_test_split(xn_t, tn_t, train_size=size_u_n)
+    xu = np.concatenate([xup, xun], axis=0)
+
+    P = xp
+    U = xu
+
+    return P,U
+
+def computeCostMatrix(P,U):
+    M = sp.spatial.distance.cdist(U, P)
+    return M
+
+def sinkhornTransport(n_unl,n_pos,lambd,M):
+    p = ot.unif(n_unl)
+    q = ot.unif(n_pos)
+    #Gs = ot.sinkhorn(p, q, M, lambd, numItermax=2000,verbose=True)
+    Gs = ot.sinkhorn(p, q, M, lambd, numItermax=2000)
+    return Gs
+
+def computeEntropy(Gs):
+    #Gs = Gs.T
+    entro = np.zeros(Gs.shape[0])
+    for i in range(Gs.shape[0]):
+        entro[i] = sp.stats.entropy(Gs[i])
+        
+    entro_min = min(entro)
+    entro_max = max(entro)
+    
+    entro = (entro-entro_min)/(entro_max-entro_min)
+    return entro
+
